551530a89806d9f0c32ed1fc4aa22d3138d26b22
maike reis <maikerdralcantara@gmail.com>
2025-10-06 21:10:35 -0300

    feat(domain): add AddressSpecies value object

--- src/addresses/domain/value_objects.py
+++ src/addresses/domain/value_objects.py
@@ -8,3 +8,14 @@ class GeocodingLevel(Enum):
     FACE = 4
     LOCALITY = 5
     SECTOR = 6
+
+
+class AddressSpecies(Enum):
+    RESIDENTIAL = 1
+    COLLECTIVE_DWELLING = 2
+    AGRICULTURAL = 3
+    EDUCATIONAL = 4
+    HEALTHCARE = 5
+    OTHER_ESTABLISHMENT = 6
+    UNDER_CONSTRUCTION = 7
+    RELIGIOUS = 8
--- tests/domain/test_value_objects.py
+++ tests/domain/test_value_objects.py
@@ -1,6 +1,6 @@
 import pytest
 
-from addresses.domain.value_objects import GeocodingLevel
+from addresses.domain.value_objects import AddressSpecies, GeocodingLevel
 
 
 def test_geocoding_level_values():
@@ -29,3 +29,35 @@ def test_invalid_geocoding_enum_lookup_by_value():
 def test_invalid_geocoding_enum_access_by_name():
     with pytest.raises(AttributeError):
         _ = GeocodingLevel.UNKNOWN
+
+
+def test_address_species_values():
+    assert AddressSpecies.RESIDENTIAL.value == 1
+    assert AddressSpecies.COLLECTIVE_DWELLING.value == 2
+    assert AddressSpecies.AGRICULTURAL.value == 3
+    assert AddressSpecies.EDUCATIONAL.value == 4
+    assert AddressSpecies.HEALTHCARE.value == 5
+    assert AddressSpecies.OTHER_ESTABLISHMENT.value == 6
+    assert AddressSpecies.UNDER_CONSTRUCTION.value == 7
+    assert AddressSpecies.RELIGIOUS.value == 8
+
+
+def test_address_species_names():
+    assert AddressSpecies.RESIDENTIAL.name == "RESIDENTIAL"
+    assert AddressSpecies.COLLECTIVE_DWELLING.name == "COLLECTIVE_DWELLING"
+    assert AddressSpecies.AGRICULTURAL.name == "AGRICULTURAL"
+    assert AddressSpecies.EDUCATIONAL.name == "EDUCATIONAL"
+    assert AddressSpecies.HEALTHCARE.name == "HEALTHCARE"
+    assert AddressSpecies.OTHER_ESTABLISHMENT.name == "OTHER_ESTABLISHMENT"
+    assert AddressSpecies.UNDER_CONSTRUCTION.name == "UNDER_CONSTRUCTION"
+    assert AddressSpecies.RELIGIOUS.name == "RELIGIOUS"
+
+
+def test_invalid_address_species_enum_lookup_by_value():
+    with pytest.raises(ValueError):
+        AddressSpecies(99)
+
+
+def test_invalid_address_species_enum_access_by_name():
+    with pytest.raises(AttributeError):
+        _ = AddressSpecies.NON_EXISTENT
